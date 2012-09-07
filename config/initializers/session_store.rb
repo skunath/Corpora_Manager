87d1722c04722596fd90d38d677463f6ad4154cc
S Kunath <skunath@gmail.com>
2012-09-07 02:32:56 -0400

    initial

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CorporaManager::Application.config.session_store :cookie_store, key: '_Corpora_Manager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CorporaManager::Application.config.session_store :active_record_store
